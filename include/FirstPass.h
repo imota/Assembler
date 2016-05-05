0b9d75b307b5e58b3167ead0ad847e93e01427e2
Anderson Zago <dedezago@gmail.com>
2016-05-05 16:17:15 -0700

    Fixed the vector of Tokens*

--- include/FirstPass.h
+++ include/FirstPass.h
@@ -5,21 +5,24 @@
 #include<vector>
 #include<string>
 #include "PreProcessor.h"
-
+#include "token.h"
 
 struct SimbleTableElement {
+	SimbleTableElement(std::string s, uint n, bool r) : name(s), value(n), relative(r) {}
 	std::string name;
 	uint value;
 	bool relative;
 };
 
 struct DefinitionTableElement{
+	DefinitionTableElement(std::string s, uint a) : name(s), address(a) {}
 	std::string name;
 	uint address;
 
 };
 
 struct UseTableElement {
+	UseTableElement(std::string s, uint a) : name(s), sumAddress(a) {}
 	std::string name;
 	uint sumAddress;
 };
@@ -28,14 +31,20 @@ class FirstPass {
 public:
 	static FirstPass& instance();
 	~FirstPass();
+	std::vector<SimbleTableElement>& getSimbleTable();
+	std::vector<DefinitionTableElement>& getDefinitionTable();
+	std::vector<UseTableElement>& getUseTable();
+	void makePass(std::vector<Token*>& tks);
+
 
 private:
-	std::vector<*Token> textFile;
+	std::vector<Token*> tokens;
 	std::vector<SimbleTableElement> simbleTable;
 	std::vector<DefinitionTableElement> definitionTable;
 	std::vector<UseTableElement> useTable;
 
 	FirstPass();
+	void writeSimbleTable();
 
 };
 #endif
--- source/FirstPass.cpp
+++ source/FirstPass.cpp
@@ -9,3 +9,17 @@ FirstPass& FirstPass::instance() {
 	static FirstPass fp;
 	return fp;
 }
+
+std::vector<SimbleTableElement>& FirstPass::getSimbleTable() { return simbleTable; }
+std::vector<DefinitionTableElement>& FirstPass::getDefinitionTable() { return definitionTable; }
+std::vector<UseTableElement>& FirstPass::getUseTable() { return useTable; }
+
+void FirstPass::makePass(std::vector<Token*>& tks){
+	tokens = tks;
+	writeSimbleTable();
+}
+
+void FirstPass::writeSimbleTable(){
+
+
+}
