868e21d11481f2ff66b3e79f8e43014bad7c9eba
Anderson Zago <dedezago@gmail.com>
2016-06-23 08:13:16 -0700

    Main now takes arguments from the command line.

--- main.cpp
+++ main.cpp
@@ -11,50 +11,19 @@
 #include "keywords.h"
 #include "translator2.h"
 
-std::string chooseInputFile() {
-	int option;
-	std::string fname;
-	std::cout << "Assemble file: \n[1] Type in the file name\n[2] Go to Linking menu\nOption: ";
-	std::cin >> option;
-	switch(option){
-		case 1:
-			std::cin >> fname;
-			return fname;
-		default:
-			std::cout << "No program assembled.\n";
-			return "";
-	}	
-}
+int main(int argc, char* argv[]) {
+	if(argc ==3){
+		std::string frname(argv[1]);
+		std::string foutname(argv[2]);
+		std::vector<LineOfFile> vector_of_elements = PreProcessor::instance().preProcessFile(frname);
+		std::vector<Token> parsed_str = Parser::instance().Parse(vector_of_elements);
 
-std::string chooseOutputFile(){
-	std::string name;
-	std::cout << "Type the name of the output file: ";
-	std::cin >> name;
-	name.insert(0, "../assembled/");
-	return name;
-}
+		for(int i = 0; i < parsed_str.size(); i++)
+			parsed_str[i] = TokenCreator::instance().identifyTokenType(parsed_str[i], Invented);
 
-int main() {
-	std::string frname = "../TestFiles/zerinho.asm";
-	std::string foutname = "ia32.s";
-
-	std::vector<LineOfFile> vector_of_elements = PreProcessor::instance().preProcessFile(frname);
-	std::vector<Token> parsed_str = Parser::instance().Parse(vector_of_elements);
-
-	for(int i = 0; i < parsed_str.size(); i++)
-		parsed_str[i] = TokenCreator::instance().identifyTokenType(parsed_str[i], Invented);
-
-	//Translator t;
-	//parsed_str = t.translate(parsed_str);
-	
-	/*for(int i = 0; i < parsed_str.size(); i++)
-		parsed_str[i] = TokenCreator::instance().identifyTokenType(parsed_str[i], IA32);
-
-	for (int i=0;i<parsed_str.size();i++) {
-		std::cout << parsed_str[i].name << " is " << parsed_str[i].Type() << std::endl;
-	}*/
-
-	Translator2::instance().translate(parsed_str, foutname);
-		
+		Translator2::instance().translate(parsed_str, foutname);
+	}
+	else
+		std::cout << "Wrong number of arguments!\n";
 	return 0;
 }
