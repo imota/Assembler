f58a94ca8c5e66e6f7d9d96b1fa68c28a53e311e
imota <mota.icaro@gmail.com>
2016-06-25 08:52:03 -0300

    The mounter class genereates a hello world. Im gonna addapt it to our code

--- include/mounter.h
+++ include/mounter.h
@@ -1,9 +1,12 @@
 #ifndef TRAB1_SB_ZAGO_ICARO_MOUNTER
 #define TRAB1_SB_ZAGO_ICARO_MOUNTER
 
+#include <elfio/elfio.hpp>
+
 class Mounter {
   public:
   	static Mounter& instance();
+  	void mount(std::string foutname);
   private:
   	Mounter() {}
 };
--- source/mounter.cpp
+++ source/mounter.cpp
@@ -4,3 +4,50 @@ Mounter& Mounter::instance() {
 	static Mounter m;
 	return m;
 }
+
+void Mounter::mount(std::string foutname) {
+	ELFIO::elfio writter;
+	writter.create(ELFCLASS32, ELFDATA2LSB);
+	writter.set_os_abi(ELFOSABI_LINUX);
+	writter.set_type(ET_EXEC);
+	writter.set_machine(EM_386);
+
+	ELFIO::section* text_sec = writter.sections.add(".text");
+	text_sec->set_type(SHT_PROGBITS);
+	text_sec->set_flags(SHF_ALLOC | SHF_EXECINSTR);
+	text_sec->set_addr_align(0x10);
+	char text[] =	 {'\xB8', '\x04', '\x00', '\x00', '\x00',  // mov eax, 4
+					'\xBB', '\x01', '\x00', '\x00', '\x00',   // mov ebx, 1
+					'\xB9', '\x20', '\x80', '\x04', '\x08',   // mov ecx, msg 
+					'\xBA', '\x0E', '\x00', '\x00', '\x00',   // mov edx, 14 
+					'\xCD', '\x80',                           // int 0x80
+					'\xB8', '\x01', '\x00', '\x00', '\x00',   // mov eax, 1
+					'\xCD', '\x80'};						  // int 0x80
+	text_sec->set_data(text, sizeof(text));
+	ELFIO::segment* text_seg = writter.segments.add();
+	text_seg->set_type(PT_LOAD);
+	text_seg->set_virtual_address(0x8048000);
+	text_seg->set_physical_address(0x8048000);
+	text_seg->set_flags(PF_X | PF_R);
+	text_seg->set_align(0x1000);
+	text_seg->add_section_index(text_sec->get_index(), text_sec->get_addr_align());
+
+	ELFIO::section* data_sec = writter.sections.add(".data");
+	data_sec->set_type(SHT_PROGBITS);
+	data_sec->set_flags(SHF_ALLOC | SHF_WRITE);
+	data_sec->set_addr_align(0x4);
+	char data[] = {	'\x48', '\x65', '\x6C', '\x6C', '\x6F',// “Hello, World!\n”
+					'\x2C', '\x20', '\x57', '\x6F', '\x72',
+					'\x6C', '\x64', '\x21', '\x0A'};
+	data_sec->set_data(data,sizeof(data));
+	ELFIO::segment* data_seg = writter.segments.add();
+	data_seg->set_type(PT_LOAD);
+	data_seg->set_virtual_address(0x08048020);
+	data_seg->set_physical_address(0x8048020);
+	data_seg->set_flags(PF_W | PF_R);
+	data_seg->set_align(0x10);
+	data_seg->add_section_index(data_sec->get_index(), data_sec->get_addr_align());
+
+	writter.set_entry(0x08048000);
+	writter.save("hello_i386_32");
+}
